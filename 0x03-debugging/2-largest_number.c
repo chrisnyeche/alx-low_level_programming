7fa35d1e206e9458c872fa7949a414de06ad8a7b
chrisdesignsofficial <chrisdesigns2021@gmail.com>
2022-09-14 22:06:02 +0100

    latest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -12,21 +12,17 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
-	largest = a;
+largest = a;
 }
-else if (b > c && b > a)
+else if (b >= a && b >= c)
 {
-	largest = b;
-}
-else if (c > b)
-{
-	largest = c;
+largest = b;
 }
 else
 {
-	largest = b;
+largest = c;
 }
 
 return (largest);
--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
 
 int main(void)
 {
-	int month;
-	int day;
-	int year;
+int month;
+int day;
+int year;
 
-	month = 10;
-	day = 4;
-	year = 1997;
+month = 2;
+day = 29;
+year = 2000;
 
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
+printf("Date: %02d/%02d/%04d\n", month, day , year);
 
-	day = convert_day(month, day);
+day = convert_day(month, day);
 
-	print_remaining_days(month, day, year);
+print_remaining_days(month, day, year);
 
-	return (0);
+return (0);
 }
