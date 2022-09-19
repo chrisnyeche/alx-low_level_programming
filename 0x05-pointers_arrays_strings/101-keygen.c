5a0d3cd5c10dc65610292cdf8e75f45cbc3fceb2
chrisdesignsofficial <chrisdesigns2021@gmail.com>
2022-09-20 00:35:56 +0100

    update key gen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,36 @@
-#include "main.h"
+include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#define stopLimit 2645
+#define asciiStop 127
+#define asciiStart 32
+
 /**
+ * main - generates keygen (a random password)
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-int num;
+char password[100];
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+	int randValue, num = 0, i = 0;
+srand(time(NULL));
+while (num < stopLimit)
+	{
+		randValue = random() % asciiStop;
+if (randValue > asciiStart)
+		{
+			password[i++] = randValue;
+																				num += randValue;
+		}
+password[i++] = randValue;
+																				num += randValue;
+		}
+
+	}
+	password[i++] = (2772 - num);
+password[i] = '\0';
+printf("%s", password);
 return (0);
 }
