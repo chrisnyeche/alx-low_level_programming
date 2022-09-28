72d591a1e7e9b9bef36aa469afff9bcc96aa716b
chrisdesignsofficial <chrisdesigns2021@gmail.com>
2022-09-28 21:43:47 +0100

    switch recursuon

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _puts_recursion - a function that prints a new line
- *
- * @s: the string
- *
- * Return: nothing
-*/
+ *_puts_recursion - prints a string
+ *@s: pointer block of memory to fill
+ *Return: void
+ */
 
 void _puts_recursion(char *s)
+
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+
+_putchar (*s);
+_puts_recursion(s + 1);
+
 }
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,19 @@
+#include "main.h"
 /**
- * factorial - a function that finds the factorial of *n
- *
- * @n: positive number
- *
- * Return: factorial of @n
-*/
+ *factorial - factorial of a given number
+ *@n: pointer block of memory to fill
+ *Return: factorial
+ */
+
 int factorial(int n)
 {
-	if (n < 0)		/*error -1 if number is negative*/
-		return (-1);
-	else if (n == 0)	/*return factorial of 0 = 1*/
-		return (1);
-	else			/*do factorial of n if its not less or equal to 0*/
-		return (n * factorial(n - 1));
+if (n == 0) /*Base condition*/
+
+return (1);
+
+else if (n < 0) /*Base condition*/
+return (-1);
+else
+return (n * factorial(n - 1)); /*Recursive call*/
+
 }
