91fd6336eb60166c1ba3d909253acb2fd4ceb8e4
chrisdesignsofficial <chrisdesigns2021@gmail.com>
2022-10-04 22:21:31 +0100

    malloc

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -1,37 +1,23 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
+ * main - check the code for ALX School students.
  *
- * Return: nothing
- */
-void print_tab(char **tab)
+ * Return: Always 0.
+*/
+
+int main(int ac, char *av[])
 {
-    int i;
+	char *s;
 
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
-int main(void)
-{
-    char **tab;
-    printf("test\n");
-    tab = strtow("Holberton School         #cisfun");
-
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
 }
